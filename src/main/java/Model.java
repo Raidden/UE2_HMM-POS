cb36882c40c34f9003f37e283fba310ad477f1d0
Raidden <33195755+Raidden@users.noreply.github.com>
2017-12-09 02:28:32 +0100

    Add files via upload

--- src/main/java/Model.java
+++ src/main/java/Model.java
@@ -2,51 +2,57 @@ import java.util.TreeMap;
 
 public class Model {
 
-	private TreeMap<String, Double> tagFrequency;
-	private TreeMap< String , TreeMap<String , Double>> prevActualMap;
-	private TreeMap< String , TreeMap<String , Double>> tagTockenMap;
-	private String[] brownCorpusTags;
-	
+    private TreeMap<String, Double> tagFrequency;
+    private TreeMap<String, TreeMap<String, Double>> prevActualMap;
+    private TreeMap<String, TreeMap<String, Double>> tagTockenMap;
+    private String[] brownCorpusTags;
 
 
-	public Model() {
-		super();
-	}
-	
-	public Model(TreeMap<String, Double> tagFrequency, TreeMap<String, TreeMap<String, Double>> prevActualMap,
-			TreeMap<String, TreeMap<String, Double>> tagTockenMap, String[] brownCorpusTags) {
-		super();
-		this.tagFrequency = tagFrequency;
-		this.prevActualMap = prevActualMap;
-		this.tagTockenMap = tagTockenMap;
-		this.brownCorpusTags = brownCorpusTags;
-	}
-	
-	
-	public TreeMap<String, Double> getTagFrequency() {
-		return tagFrequency;
-	}
-	public void setTagFrequency(TreeMap<String, Double> tagFrequency) {
-		this.tagFrequency = tagFrequency;
-	}
-	public TreeMap<String, TreeMap<String, Double>> getPrevActualMap() {
-		return prevActualMap;
-	}
-	public void setPrevActualMap(TreeMap<String, TreeMap<String, Double>> prevActualMap) {
-		this.prevActualMap = prevActualMap;
-	}
-	public TreeMap<String, TreeMap<String, Double>> getTagTockenMap() {
-		return tagTockenMap;
-	}
-	public void setTagTockenMap(TreeMap<String, TreeMap<String, Double>> tagTockenMap) {
-		this.tagTockenMap = tagTockenMap;
-	}	
-	public String[] getBrownCorpusTags() {
-		return brownCorpusTags;
-	}
-	public void setBrownCorpusTags(String[] brownCorpusTags) {
-		this.brownCorpusTags = brownCorpusTags;
-	}
-	
-	
+    public Model() {
+        super();
+    }
+
+    public Model(TreeMap<String, Double> tagFrequency, TreeMap<String, TreeMap<String, Double>> prevActualMap,
+                 TreeMap<String, TreeMap<String, Double>> tagTockenMap, String[] brownCorpusTags) {
+        super();
+        this.tagFrequency = tagFrequency;
+        this.prevActualMap = prevActualMap;
+        this.tagTockenMap = tagTockenMap;
+        this.brownCorpusTags = brownCorpusTags;
+    }
+
+
+    public TreeMap<String, Double> getTagFrequency() {
+        return tagFrequency;
+    }
+
+    public void setTagFrequency(TreeMap<String, Double> tagFrequency) {
+        this.tagFrequency = tagFrequency;
+    }
+
+    public TreeMap<String, TreeMap<String, Double>> getPrevActualMap() {
+        return prevActualMap;
+    }
+
+    public void setPrevActualMap(TreeMap<String, TreeMap<String, Double>> prevActualMap) {
+        this.prevActualMap = prevActualMap;
+    }
+
+    public TreeMap<String, TreeMap<String, Double>> getTagTockenMap() {
+        return tagTockenMap;
+    }
+
+    public void setTagTockenMap(TreeMap<String, TreeMap<String, Double>> tagTockenMap) {
+        this.tagTockenMap = tagTockenMap;
+    }
+
+    public String[] getBrownCorpusTags() {
+        return brownCorpusTags;
+    }
+
+    public void setBrownCorpusTags(String[] brownCorpusTags) {
+        this.brownCorpusTags = brownCorpusTags;
+    }
+
+
 }
--- src/main/java/Writer.java
+++ src/main/java/Writer.java
@@ -5,30 +5,30 @@ import java.util.ArrayList;
 
 public class Writer {
 
-	//TODO: create a Methode that writes the Results in txt-data
-	public void writeResult(Tagger tagger , ArrayList<String> result , String[] sentence ,PrintWriter writer) throws FileNotFoundException {
-		// TODO Auto-generated method stub
-		
-		
-		try {
-			
-			String line="";
-			for (int i = 0; i < sentence.length; i++) {
-				
-					line += sentence[i].replace(sentence[i].substring(sentence[i].indexOf("/"), sentence[i].length())
-							, "/"+result.get(i))+" ";	
-				
-				}
-			
-		if (line.contains("./.")) {
-			writer.append((line.trim()+"\n"));
-		}else {
-			writer.append((line.trim()));
-		}
-		} catch (Exception e) {
-			// TODO: handle exception
-		}
-		
-		
-	}
+    //TODO: create a Methode that writes the Results in txt-data
+    public void writeResult(Tagger tagger, ArrayList<String> result, String[] sentence, PrintWriter writer) throws FileNotFoundException {
+        // TODO Auto-generated method stub
+
+
+        try {
+
+            String line = "";
+            for (int i = 0; i < sentence.length; i++) {
+
+                line += sentence[i].replace(sentence[i].substring(sentence[i].lastIndexOf("/"), sentence[i].length())
+                        , "/" + result.get(i)) + " ";
+
+            }
+
+            if (line.contains("./.")) {
+                writer.append(line + "\n");
+            } else {
+                writer.append(line);
+            }
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+
+
+    }
 }
